6096ed3427cf8b99523b86aabd129fc468719068
Benn <bennkiche7@gmail.com>
2024-12-18 01:37:30 +0300

    updating models

---
+++ models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column,Integer,String,ForeignKey
+from sqlalchemy.orm import relationship,declarative_base
+
+Base = declarative_base()
+
+class LAB(Base):
+    __tablename__ = "labs"
+    id = Column(Integer,primary_key= True)
+    name = Column(String,nullable=False)
+    email = Column(String,unique=True,nullable=False)
+
+    tests = relationship("Test",back_populates="lab")
+
+    def __repr__(self):
+        return f"LAB(id = {self.id}, name= '{self.name}', email = '{self.email}')"
+    
+
+class Test(Base):
+    __tablename__="tests"
+    id = Column(Integer,primary_key=True)
+    name = Column(String,nullable=False)
+    price = Column(Integer,nullable=False)
+    lab_id = Column(Integer,ForeignKey("labs.id"))
+
+    lab = relationship("LAB",back_populates="tests")  
+
+    def __repr__(self):
+        return f"Test(id= {self.id}, name = '{self.name}',price = {self.price}, lab_id = {self.lab_id})"
